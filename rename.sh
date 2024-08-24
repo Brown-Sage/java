cc607495ee2975a4e47971b94a17cc386920510e
Aaryan <luckytak60@gmail.com>
2024-08-24 18:38:08 +0530

    renaming again because i messed up

--- rename.sh
+++ rename.sh
@@ -1,90 +1,26 @@
 #!/bin/bash
 
-# Array of ordered file names
-files=(
-    "helloworld.java"
-    "increment.java"
-    "forloop.java"
-    "whileloop.java"
-    "dowhileloop.java"
-    "ifelse.java"
-    "breakinloop.java"
-    "continueinloop.java"
-    "typecasting1.java"
-    "array.java"
-    "arrayloop.java"
-    "multidimensionalarr.java"
-    "method.java"
-    "method1.java"
-    "method3.java"
-    "Constructor.java"
-    "inheritancebyme.java"
-    "inheritancebyme2.java"
-    "methodoverriding.java"
-    "abstractclass.java"
-    "interfaces.java"
-    "DynamicMethoDispatch.java"
-    "polymorphismininterfaces.java"
-    "cwh_45_inheritance.java"
-    "cwh_59_polymorphism.java"
-    "multithreading.java"
-    "threads.java"
-    "threadbyrunnableinterface.java"
-    "cwh_73_thread_constructor.java"
-    "trycatch.java"
-    "nestedtrycatch.java"
-    "finallyfunc.java"
-    "rockpaper.java"
-    "random.java"
-    "random2.java"
-    "random3.java"
-    "random4.java"
-    "random6.java"
-    "random7.java"
-    "random8.java"
-    "random9.java"
-    "random10.java"
-    "random11.java"
-    "random12.java"
-    "random13.java"
-    "random45.java"
-    "date.java"
-    "date1.java"
-    "string.java"
-    "pyramid.java"
-    "varArgs.java"
-    "base.java"
-    "constructorinineheri.java"
-    "college.java"
-    "allinonepackage.java"
-    "ArrayLista.java"
-    "Arraylist.java"
-    "arraylist1.java"
-    "arraydeque1.java"
-    "fbvfshjmkh.java"
-    "bkchodi.java"
-    "dk.java"
-    "tempCodeRunnerFile.java"
-    "infloop.java"
-    "multiplevarinloop.java"
-    "Recursion.java"
-)
+# Specify the directory where your Java files are located
+directory=$pwd
 
-# Directory where your Java files are located
-directory="/home/aryan/Programming/java"
-
-# Change to the directory
+# Change to the specified directory
 cd "$directory" || exit
 
-# Rename each file with a number prefix
-for i in "${!files[@]}"; do
-    original_file="${files[$i]}"
-    new_file="$((i + 1))${original_file}"
+# Loop through all .java files in the directory
+for file in *.java; do
+    # Check if the file starts with a digit
+    if [[ $file =~ ^[0-9] ]]; then
+        # Extract the number and the rest of the filename
+        number="${file%%[!0-9]*}"
+        rest="${file#$number}"
+        
+        # Form the new filename
+        new_filename="j${number}_${rest}"
 
-    if [[ -f "$original_file" ]]; then
-        mv "$original_file" "$new_file"
-        echo "Renamed $original_file to $new_file"
-    else
-        echo "File $original_file does not exist, skipping..."
+        # Rename the file
+        mv "$file" "$new_filename"
+
+        # Print the renamed file
+        echo "Renamed: $file -> $new_filename"
     fi
 done
